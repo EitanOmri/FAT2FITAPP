65d524ba8a9bea4ecf7b372525d65ccfb6a676b8
omri eitan <omrieitan@icloud.com>
2019-01-05 09:01:07 +0200

    add web view

--- app/src/main/java/com/fat2fit/fat2fitapp/MainActivity.java
+++ app/src/main/java/com/fat2fit/fat2fitapp/MainActivity.java
@@ -2,12 +2,26 @@ package com.fat2fit.fat2fitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        WebView ob=new WebView(this);
+        ob.getSettings().setJavaScriptEnabled(true);
+        ob.loadUrl("https://www.google.com/");
+        //opens all link in the same web view instead of the default browser
+        ob.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return false;
+            }
+        });
+        setContentView(ob);
     }
 }
